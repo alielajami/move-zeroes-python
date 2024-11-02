a28301630a61629f20c1ebcab4408d9be1d0af68
alielajami <alielajami@outlook.com>
2024-11-02 20:38:33 +0200

    commenting for clean code

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 ''' Application for moving zeroes to the end of a list '''
 
-array_size = int(input("Enter the size of the array: "))
-integers_array = []
+array_size = int(input("Enter the size of the array: ")) # Getting the size of the array
+integers_array = [] # Empty list to store the integers
 
-for i in range(array_size):
-    integers_array.append(int(input(f"Enter the integer {i+1}: ")))
+for i in range(array_size): # Getting the integers from the user
+    integers_array.append(int(input(f"Enter the integer {i+1}: "))) # Appending the integers
 
 def move_zeroes(array):
     ''' Function to move zeroes to the end of the list '''
-    for j in range(array.count(0)):
-        array.remove(0)
-        array.append(0)
-    return array
+    for j in range(array.count(0)): # Counting the number of zeroes in the
+        array.remove(0) # Removing the zeroes from the list
+        array.append(0) # Appending the zeroes to the end of the list
+    return array # Returning the list
 
-print(move_zeroes(integers_array))
+print(move_zeroes(integers_array)) # Printing the list after moving the zeroes to the end
