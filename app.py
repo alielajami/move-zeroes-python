1d722512eceaace1ad64804669db8b33625f0604
alielajami <alielajami@outlook.com>
2024-11-02 20:34:58 +0200

    making the code simpler by using .remove .count .append

--- app.py
+++ app.py
@@ -7,17 +7,10 @@ for i in range(array_size):
     integers_array.append(int(input(f"Enter the integer {i+1}: ")))
 
 def move_zeroes(array):
-    ''' Function to move zeroes to the end of the list without using .count and .remove '''
-    non_zero_index = 0
-
-    for j in range(len(array)):
-        if array[j] != 0:
-            array[non_zero_index] = array[j]
-            non_zero_index += 1
-
-    for k in range(non_zero_index, len(array)):
-        array[k] = 0
-    
+    ''' Function to move zeroes to the end of the list '''
+    for j in range(array.count(0)):
+        array.remove(0)
+        array.append(0)
     return array
 
 print(move_zeroes(integers_array))
